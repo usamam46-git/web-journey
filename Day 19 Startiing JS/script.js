8117e657afee84fe8d979505d5b4d611e267d173
usamam46-git <usamam46@gmail.com>
2025-08-25 23:55:41 +0500

    Adding Day 19

---
+++ Day 19 Startiing JS/script.js
@@ -0,0 +1,30 @@
+// alert("Hello World");
+// let a= prompt("Enter your Number")
+// let istrue = confirm("Are you sure you want to leave this page and blast your computer?")
+// if (istrue){
+//     console.log("Computer is ready to blast")
+// }
+console.log("Code is running")
+let c= "usama";
+const p= false;
+let n= undefined;
+let m= null;
+let z=321
+console.log(typeof c)
+console.log(typeof n)
+console.log(typeof m)
+console.log(typeof z)
+document.title= "Learning JS"
+
+let o={
+    "name":"Usama",
+    "Job Role":"5600",
+    "Salary" : "100000"
+}
+console.log(o)
+let f={
+    name: "Usama",
+    "Phone Number": "03064423884",
+    "Marks": 400
+}
+console.log(f)

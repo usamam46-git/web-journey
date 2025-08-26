73df7091537a7c828d56c551b5d7955b01956a0a
usamam46-git <usamam46@gmail.com>
2025-08-26 22:49:10 +0500

    Adding Day 21

---
+++ Day 21 with JS/index.js
@@ -0,0 +1,29 @@
+// console.log("Hello I'm conditional Tutorial")
+// console.log("Hello I'm conditional Tutorial")
+let a = 2;
+// let grace = 17;
+// console.log(age % grace)
+
+// if ((age*grace)<= 18) {
+//     console.log("You can Drive")
+// }
+// else { console.log("you cannot drive") }
+
+// console.log(age+grace)
+// console.log(age-grace)
+// if(age==18){console.log("You cannot drive")
+// }
+
+//     else if(age==0){console.log("Are you kidding?")}
+// else{console.log("Are you still kiddin?")}
+let b= 5;
+
+let c= b>a? (b-a):(a-b);
+console.log(typeof c)
+
+
+
+
+
+
+
---
+++ Day 21 with JS/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+console.log("Hello I'm conditional Tutorial")
+// console.log("Hello I'm conditional Tutorial")

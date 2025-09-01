a0d7786a0cf7375d67092cf9758e9880bbff0bd9
usamam46-git <usamam46@gmail.com>
2025-09-02 02:32:42 +0500

    Day 28

---
+++ Day 28/script.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+console.log(document.body.childNodes);
+console.log(document.body.childNodes[1]);
+let cont= document.body.childNodes[1];
+console.log(cont.firstChild);
+console.log(cont.firstElementChild);
+console.log(document.body.lastChild.children);
+console.log(typeof document);
+
+console.log(typeof window);
